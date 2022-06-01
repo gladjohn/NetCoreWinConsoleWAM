c5b7fea57d6513c26baa92188614851175ec8ff0
Gladwin Johnson <gljohns@microsoft.com>
2022-05-31 21:47:30 -0700

    Test Updates

--- WAMTest/UnitTest1.cs
+++ WAMTest/UnitTest1.cs
@@ -1,3 +1,4 @@
+using Microsoft.Identity.Client.NativeInterop;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NetCoreWinConsoleWAM;
 using System.Threading.Tasks;
@@ -8,11 +9,27 @@ namespace WAMTest
     public class UnitTest1
     {
         [TestMethod]
-        public async Task TestMethod1()
+        public async Task ValidateAcquireTokenSilentlyAsync()
         {
-            WAMApp wamApp = new WAMApp();
-            
+            AuthResult authResult = await WAMApp.WAMValidate();
 
+            Assert.IsNotNull(authResult.Account.Id);
+
+            AuthResult result = await WAMApp.ValidateAcquireTokenSilentlyAsync(authResult.Account);
+
+            Assert.AreEqual(authResult.Account, result.Account);
+        }
+
+        [TestMethod]
+        public async Task ValidateAcquireTokenInteractivelyAsync()
+        {
+            AuthResult authResult = await WAMApp.WAMValidate();
+
+            Assert.IsNotNull(authResult.Account.Id);
+
+            AuthResult result = await WAMApp.ValidateAcquireTokenInteractivelyAsync(authResult.Account);
+
+            Assert.AreEqual(authResult.Account, result.Account);
         }
     }
 }
